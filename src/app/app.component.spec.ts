6f0b05dd5f6ca878b8e29e2f613ef0ea8bab906b
Alex Botelho <alexbotelho@ALEX01.hallyzsystem.com>
2021-07-13 10:04:30 -0300

    Initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'y'`, () => {
+  it(`should have as title 'Olá Mundo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('y');
+    expect(app.title).toEqual('Olá Mundo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('y app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Olá Mundo app is running!');
   });
 });
